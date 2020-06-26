894fec4f06b1128a81fca988d25cf0163429bcdb
tuannt2909 <nguyenanhtuan29091997@gmail.com>
2020-06-26 16:50:09 +0700

    first commit

---
+++ GTSPrj/GSTLiteProj/src/main/java/com/GSTLiteProj/GstLiteProjApplication.java
@@ -0,0 +1,13 @@
+package com.GSTLiteProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstLiteProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GstLiteProjApplication.class, args);
+	}
+
+}
---
+++ GTSPrj/GSTLiteProj/src/test/java/com/GSTLiteProj/GstLiteProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GSTLiteProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GstLiteProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
